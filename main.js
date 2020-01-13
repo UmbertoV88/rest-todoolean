edbd203baad0dca5f9ec188a34b4908036b98095
UmbertoV <umberto.ottone@gmail.com>
2020-01-13 15:30:12 +0100

    recuerato i dati della lista con chiamata ajax

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
     // faccio la chiamata ajax per prendere i dati della Lista
-    
+    $.ajax({
+        url:"http://157.230.17.132:3022/todos",
+        method: "GET",
+        success: function(data){
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                console.log(data[i]);
+                var testo_todo = data[i].text;
+                console.log(testo_todo);
+            }
+        },
+        error:function(){
+            alert("ERRORE")
+        }
+    });
 });
